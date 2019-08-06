b247e0960cd4042fe59a67ef3770054c5c4fc955
seamuss1 <seamusdgallagher@gmail.com>
2019-08-06 15:40:34 -0400

    New physics changes closer to realism

--- bouncingball_cortix.py
+++ bouncingball_cortix.py
@@ -2,8 +2,6 @@ import os, time, datetime, threading, random
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-if "C:/Users/seamu/OneDrive/Documents/GitHub/cortix" not in sys.path:
-    sys.path.append("C:/Users/seamu/OneDrive/Documents/GitHub/cortix")
 from cortix.src.module import Module
 from cortix.src.port import Port
 from cortix.src.cortix_main import Cortix
@@ -41,10 +39,16 @@ class BouncingBall(Module):
         self.v0 = [random.uniform(-50,50),random.uniform(-30,30)]
         self.cor = 0.95
         self.a = (0,-9.81)
+        self.m = 1
+        self.KE = 0.5*self.m*((self.v0[0]**2+self.v0[1]**2)**0.5)**2
         self.timestamp=str(datetime.datetime.now())
         self.messenger = Messenger()
         self.messenger.circle = self.circle
         self.messenger.timestamp = self.timestamp
+        self.messenger.m,self.messenger.r = 1,1
+        self.messenger.v = self.v0
+        self.messenger.p = self.p0
+        
     def run(self, state_comm=None, idx_comm=None):
         state_comm.put((idx_comm,self.state))
         t = 0.01
@@ -64,25 +68,28 @@ class BouncingBall(Module):
             self.v0[0] = self.a[0]*t + self.v0[0]
             self.pnt = geo.point.Point(self.p0[0],self.p0[1])
             self.circle = self.pnt.buffer(self.r)
-            self.messenger.circle = self.circle
             
             for shape in self.bndry:
                 if self.circle.crosses(shape) or self.circle.touches(shape) or self.circle.intersects(shape):
                     self.wall_collision(shape)
             for name in portdic:
+                messenger = portdic[name]
                 shape = portdic[name].circle
                 ts = portdic[name].timestamp
                 if self.circle.crosses(shape) or self.circle.touches(shape) or self.circle.intersects(shape):
-                    self.ball_shift(shape)
-                    self.ball_collision(shape)
+                    self.ball_collision(messenger)
+                    #self.ball_shift(shape)
                     self.messenger.collision.append(ts)
-                    
                     continue
                 
                 for line in portdic[name].collision:
-                    
+
                     if self.timestamp == line:
-                        self.ball_collision(shape)
+                        self.ball_collision(messenger)
+                        
+            self.messenger.circle = self.circle
+            self.messenger.v = self.v0
+            self.messenger.p = self.p0
             for i in self.ports:
                 self.send(self.messenger,i)
             for i in self.ports:
@@ -96,9 +103,8 @@ class BouncingBall(Module):
             if 'plot' in str(i):
                 self.send('done',i)
         print('done')
-        
-        
         return
+    
     def wall_collision(self,shape):
         pi,pf = list(shape.coords)
         p1,p2 = shapely.ops.nearest_points(self.pnt,shape)
@@ -124,33 +130,35 @@ class BouncingBall(Module):
         p1,p2 = shapely.ops.nearest_points(self.pnt,shape)
         angle = np.arctan2(p2.y - p1.y, p2.x - p1.x)
         d = shape.distance(self.pnt)
-        self.p0 = [self.p0[0]-(self.r-d)*np.cos(angle),self.p0[1]-(self.r-d)*np.sin(angle)]
+        self.p0 = [self.p0[0]-(self.r*1.05-d)*np.cos(angle),self.p0[1]-(self.r*1.05-d)*np.sin(angle)]
         self.pnt = geo.point.Point(self.p0[0],self.p0[1])
         self.circle = self.pnt.buffer(self.r)
         
-    def ball_collision(self,shape):
+    def ball_collision(self,messenger):
+        shape = messenger.circle
+        v2,m = messenger.v,messenger.m
+        v3 = (v2[0]**2+v2[1]**2)**0.5
+        phi = np.arctan2(v2[1],v2[0])
         p1,p2 = shapely.ops.nearest_points(self.pnt,shape)
         angle = np.arctan2(p2.y - p1.y, p2.x - p1.x)        
         angle2 = np.arctan2(self.v0[1], self.v0[0])
-        theta = angle2-angle
         v = (self.v0[0]**2+self.v0[1]**2)**0.5
-        vbi, vbj = v*np.sin(theta), v*np.cos(theta)
-        vbj = -vbj *self.cor
-        v = (vbi**2+vbj**2)**0.5
-        print('Ball collision! Velocity:', v)
-        angle4 = np.arctan2(vbj, vbi)
-        angle1 = angle4-angle
         
-        self.v0 = [np.sin(angle1)*v, np.cos(angle1)*v]
-
+        #Equation source: https://en.wikipedia.org/wiki/Elastic_collision
+        vpx=((v*np.cos(angle2- angle)*(self.m-m)+2*m*v3*np.cos(phi-angle))/(self.m+m))*np.cos(angle)+v*np.sin(angle2-angle)*np.cos(angle+np.pi/2)
+        vpy=((v*np.cos(angle2- angle)*(self.m-m)+2*m*v3*np.cos(phi-angle))/(self.m+m))*np.sin(angle)+v*np.sin(angle2-angle)*np.cos(angle+np.pi/2)
+        
+        self.v0 = [vpx,vpy]
 
 class Messenger:
     def __init__(self, circle=None, collision = [], timestamp='0'):
         self.circle = circle
         self.collision = collision
         self.timestamp = timestamp
-
-
+        self.m = 1
+        self.r = 1
+        self.v = []
+        self.p = []
         
 if __name__ == '__main__':
     cortix = Cortix(use_mpi=False)
